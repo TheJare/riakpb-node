cfa3d51cbf19b7ff6c60924f625570d916324b67
jare <jare@iguanademos.com>
2012-05-12 03:49:06 +0200

    support for more messages. Get still buggy

--- app.js
+++ app.js
@@ -31,8 +31,12 @@ var readValue = function(type, stream) {
     }
 }
 
-var decodeLoop = function(stream, cb) {
-    while (stream.idx < stream.len) {
+var decodeLoop = function(stream, cb, len) {
+    if (len)
+        len += stream.idx;
+    else
+        len = stream.len
+    while (stream.idx < len) {
         var t = stream.buf[stream.idx++];
         var fieldnum = t >> 3;
         var type = t & 0x07;
@@ -40,8 +44,18 @@ var decodeLoop = function(stream, cb) {
     }
 }
 
-var decode_RpbGetServerInfoReq  = function(stream, res) {
-    res = res || {node:"", server_version:"", done : false};
+var decode_RpbGetClientIdResp = function(stream, res) {
+    res = res || {client_id:"", done : true};
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 2) {
+            res.client_id = readString(stream);
+        }
+    });
+    return res;
+}
+
+var decode_RpbGetServerInfoResp  = function(stream, res) {
+    res = res || {node:"", server_version:"", done : true};
     decodeLoop(stream, function(type, fieldnum) {
         if (fieldnum == 1 && type == 2) {
             res.node = readString(stream);
@@ -49,18 +63,30 @@ var decode_RpbGetServerInfoReq  = function(stream, res) {
             res.server_version = readString(stream);
         }
     });
-    res.done = true;
+    return res;
+}
+
+var decode_RpbGetResp = function(stream, res) {
+    res = res || {content:[], vclock:"", unchanged:true, done : true};
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 2) {
+            res.content.push(decode_RpbContent(stream));
+        } else if (fieldnum == 2 && type == 2) {
+            res.vclock = readString(stream);
+        } else if (fieldnum == 3 && type == 0) {
+            res.unchanged = readVarInt(stream);
+        }
+    });
     return res;
 }
 
 var decode_RpbListBucketsResp = function(stream, res) {
-    res = res || {buckets:[], done : false};
+    res = res || {buckets:[], done : true};
     decodeLoop(stream, function(type, fieldnum) {
         if (fieldnum == 1 && type == 2) {
             res.buckets.push(readString(stream));
         }
     });
-    res.done = true;
     return res;
 }
 
@@ -75,6 +101,90 @@ var decode_RpbListKeysResp = function(stream, res) {
     });
     return res;
 }
+
+var decode_RpbGetBucketResp = function(stream, res) {
+    res = res || {props : {}, done : true};
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 2) {
+            res.props = decode_RpbBucketProps(stream);
+        }
+    });
+    return res;
+}
+
+var decode_RpbBucketProps = function(stream, res) {
+    var len = readVarInt(stream);
+    res = res || {n_val : 0, allow_mult: false};
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 0) {
+            res.n_val = readVarInt(stream);
+        } else if (fieldnum == 2 && type == 0) {
+            res.allow_mult = readVarInt(stream);
+        }
+    }, len);
+    return res;
+}
+
+var decode_RpbPair = function(stream, res) {
+    var len = readVarInt(stream);
+    res = res || {key:"", value : "" };
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 2) {
+            res.key = readString(stream);
+        } else if (fieldnum == 2 && type == 2) {
+            res.value = readString(stream);
+        }
+    }, len);
+    console.log("pair: ", res);
+    return res;
+}
+
+var decode_RpbLink = function(stream, res) {
+    var len = readVarInt(stream);
+    res = res || {bucket: "", key:"", tag : "" };
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 2) {
+            res.bucket = readString(stream);
+        } else if (fieldnum == 2 && type == 2) {
+            res.key = readString(stream);
+        } else if (fieldnum == 3 && type == 2) {
+            res.tag = readString(stream);
+        }
+    }, len);
+    console.log("link: ", res);
+    return res;
+}
+
+var decode_RpbContent = function(stream, res) {
+    var len = readVarInt(stream);
+    res = res || {value : "", content_type:"", charset:"", content_encoding:"", vtag:"", links:[], last_mod:0, last_mod_usecs:0, usermeta:[], indexes:[] };
+    decodeLoop(stream, function(type, fieldnum) {
+        if (fieldnum == 1 && type == 2) {
+            res.value = readString(stream);
+        } else if (fieldnum == 2 && type == 2) {
+            res.content_type = readString(stream);
+        } else if (fieldnum == 3 && type == 2) {
+            res.charset = readString(stream);
+        } else if (fieldnum == 4 && type == 2) {
+            res.content_type = readString(stream);
+        } else if (fieldnum == 5 && type == 2) {
+            res.vtag = readString(stream);
+        } else if (fieldnum == 6 && type == 2) {
+            res.links.push(decode_RpbLink(stream));
+        } else if (fieldnum == 7 && type == 0) {
+            res.last_mod = readVarInt(stream);
+        } else if (fieldnum == 8 && type == 0) {
+            res.last_mod_usecs = readVarInt(stream);
+        } else if (fieldnum == 9 && type == 2) {
+            res.usermeta.push(decode_RpbPair(stream));
+        } else if (fieldnum == 10 && type == 2) {
+            res.indexes.push(decode_RpbPair(stream));
+        }
+    }, len);
+    return res;
+}
+
+// ----------------------
 
 var addMessageHeader = function(buf, len, msg) {
     buf[0] = (len >> 24) & 0xFF;
@@ -94,6 +204,13 @@ var addParameters = function(buf, idx, params) {
             buf[idx+1] = p.length;
             buf.write(p, idx+2, p.length, 'utf8');
             idx += 2 + p.length;
+        } else if (type == "object") {
+            buf[idx] = ((i+1) << 3) + 2;
+            var tempv = new Buffer(4096);
+            var l = addParameters(tempv, 0, p);
+            buf[idx+1] = l; // TODO: write varInt
+            tempv.copy(buf, idx+2, 0, l);
+            idx += 2 + l;
         } else if (type == "boolean") {
             buf[idx] = (i+1) << 3;
             buf[idx+1] = p? 1 : 0;
@@ -111,16 +228,41 @@ var makeMessage = function(msg, params) {
     return b0.slice(0, idx);
 }
 
-var encode_ListBucketsReq = function() {
-    return new Buffer([0,0,0,1,15]);
+var encode_PingReq = function() {
+    return new Buffer([0,0,0,1,1]);
+}
+
+var encode_GetClientIdReq = function() {
+    return new Buffer([0,0,0,1,3]);
+}
+
+var encode_SetClientIdReq = function(client_id) {
+    return makeMessage(5, [client_id]);
 }
 
 var encode_GetServerInfoReq = function() {
     return new Buffer([0,0,0,1,7]);
 }
 
+var encode_GetReq = function(bucket, key, options) {
+    options = options || {};
+    return makeMessage(9, [bucket, key, options.r, options.pr, options.basic_quorum, options.notfound_ok, options.if_modified, options.head, options.deletedvclock]);
+}
+
+var encode_ListBucketsReq = function() {
+    return new Buffer([0,0,0,1,15]);
+}
+
 var encode_ListKeysReq = function(bucket) {
     return makeMessage(17, [bucket]);
+}
+
+var encode_GetBucketReq = function(bucket) {
+    return makeMessage(19, [bucket]);
+}
+
+var encode_SetBucketReq = function(bucket, props) {
+    return makeMessage(21, [bucket, [props.n_val, props.allow_mult]]);
 }
 
 
@@ -139,9 +281,16 @@ var client = net.connect(18087, 'localhost', function() { //'connect' listener
     client.setKeepAlive(true);
 
     clientWrite(client, encode_GetServerInfoReq(), function(res) { console.log("respuesta al encode_GetServerInfoReq"); });
+    clientWrite(client, encode_GetClientIdReq(), function(res) { console.log("respuesta al encode_GetClientIdReq"); });
+    clientWrite(client, encode_SetClientIdReq("Jarete"), function(res) { console.log("respuesta al encode_SetClientIdReq"); });
+    clientWrite(client, encode_GetClientIdReq(), function(res) { console.log("respuesta al encode_GetClientIdReq"); });
     clientWrite(client, encode_ListBucketsReq(), function(res) { console.log("respuesta al encode_ListBucketsReq"); });
+    clientWrite(client, encode_GetBucketReq("users"), function(res) { console.log("respuesta al encode_GetBucketReq de los users"); });
+    clientWrite(client, encode_SetBucketReq("users", {allow_mult:false, n_val:4}), function(res) { console.log("respuesta al encode_SetBucketReq de los users"); });
+    clientWrite(client, encode_GetBucketReq("users"), function(res) { console.log("respuesta al encode_GetBucketReq de los users"); });
     clientWrite(client, encode_ListKeysReq("users"), function(res) { console.log("respuesta a los users"); });
-    clientWrite(client, encode_ListKeysReq("flights"), function(res) { console.log("respuesta a los flights"); client.end(); });
+    clientWrite(client, encode_ListKeysReq("flights"), function(res) { console.log("respuesta a los flights"); });
+    clientWrite(client, encode_GetReq("flights", "KLM-5034"), function(res) { console.log("respuesta al get KLM"); client.end(); });
 });
 
 client.on('data', function(data) {
@@ -153,16 +302,28 @@ client.on('data', function(data) {
         stream.idx += 5;
         stream.len = stream.idx + len-1;
         console.log("RECEIVED: message " + msg + ", length " + len);
-        if (msg == 8) {
-            res = decode_RpbGetServerInfoReq(stream, res);
+        if (msg == 2) {
+            res = {done:true}; // nothing to decode for RpbPingResp 
+        } else if (msg == 4) {
+            res = decode_RpbGetClientIdResp(stream, res);
+        } else if (msg == 6) {
+            res = {done:true}; // nothing to decode for RpbSetClientIdResp 
+        } else if (msg == 8) {
+            res = decode_RpbGetServerInfoResp(stream, res);
+        } else if (msg == 10) {
+            res = decode_RpbGetResp(stream, res);
         } else if (msg == 16) {
             res = decode_RpbListBucketsResp(stream, res);
         } else if (msg == 18) {
             res = decode_RpbListKeysResp(stream, res);
+        } else if (msg == 20) {
+            res = decode_RpbGetBucketResp(stream, res);
+        } else if (msg == 22) {
+            res = {done:true}; // nothing to decode for RpbSetBucketResp
         } else {
             stream.idx += len;
         }
-        if (res && res.done) {
+        if (!res || res.done) {
             var cb = cbs.shift();
             if (cb) cb(res);
             console.log(res);
