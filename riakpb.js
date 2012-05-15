9c2a0ae67096bfc889e51bde8991a2f523217009
jare <jare@iguanademos.com>
2012-05-16 00:39:25 +0200

    Proper handling of socket data packets, coalescing until a complete response ived

--- riakpb.js
+++ riakpb.js
@@ -11,6 +11,8 @@ var net = require('net');
 
 // -----------------------------
 // Reading protocol buffers
+// ----------------------
+
 // Only varints in the Riak protocol buffers are bools, uint8s and uint32s.
 // So we just use readVarInt to read all of them, no need to decode floats or signed ints.
 var readVarInt = function(stream) {
@@ -49,7 +51,13 @@ var decodeLoop = function(stream, cb, len) {
 
 // -----------------------------
 // Parsing Riak responses
+// Riak API: http://wiki.basho.com/Client-Implementation-Guide.html
+// ----------------------
 
+// message RpbErrorResp {
+//     required bytes errmsg = 1;
+//     required uint32 errcode = 2;
+// }
 var decode_ErrorResp  = function(stream, res) {
     res = res || {errmsg:"", errcode:0};
     decodeLoop(stream, function(type, fieldnum) {
@@ -62,6 +70,9 @@ var decode_ErrorResp  = function(stream, res) {
     return res;
 }
 
+// message RpbGetClientIdResp {
+//     required bytes client_id = 1; // Client id in use for this connection
+// }
 var decode_GetClientIdResp = function(stream, res) {
     res = res || {client_id:""};
     decodeLoop(stream, function(type, fieldnum) {
@@ -72,6 +83,10 @@ var decode_GetClientIdResp = function(stream, res) {
     return res;
 }
 
+// message RpbGetServerInfoResp {
+//     optional bytes node = 1;
+//     optional bytes server_version = 2;
+// }
 var decode_GetServerInfoResp  = function(stream, res) {
     res = res || {node:"", server_version:""};
     decodeLoop(stream, function(type, fieldnum) {
@@ -84,6 +99,11 @@ var decode_GetServerInfoResp  = function(stream, res) {
     return res;
 }
 
+// message RpbGetResp {
+//     repeated RpbContent content = 1;
+//     optional bytes vclock = 2;
+//     optional bool unchanged = 3;
+// }
 var decode_GetResp = function(stream, res) {
     res = res || {content:[], vclock:"", unchanged:true};
     decodeLoop(stream, function(type, fieldnum) {
@@ -98,6 +118,11 @@ var decode_GetResp = function(stream, res) {
     return res;
 }
 
+// message RpbPutResp {
+//     repeated RpbContent contents = 1;
+//     optional bytes vclock = 2;        // the opaque vector clock for the object
+//     optional bytes key = 3;           // the key generated, if any
+// }
 var decode_PutResp = function(stream, res) {
     res = res || {content:[], vclock:"", key:""};
     decodeLoop(stream, function(type, fieldnum) {
@@ -112,6 +137,9 @@ var decode_PutResp = function(stream, res) {
     return res;
 }
 
+// message RpbListBucketsResp {
+//     repeated bytes buckets = 1;
+// }
 var decode_ListBucketsResp = function(stream, res) {
     res = res || {buckets:[]};
     decodeLoop(stream, function(type, fieldnum) {
@@ -122,6 +150,10 @@ var decode_ListBucketsResp = function(stream, res) {
     return res;
 }
 
+// message RpbListKeysResp {
+//     repeated bytes keys = 1;
+//     optional bool done = 2;
+// }
 var decode_ListKeysResp = function(stream, res) {
     res = res || {keys:[], done : false};
     decodeLoop(stream, function(type, fieldnum) {
@@ -134,6 +166,13 @@ var decode_ListKeysResp = function(stream, res) {
     return res;
 }
 
+// message RpbGetBucketResp {
+//     required RpbBucketProps props = 1;
+// }
+// message RpbBucketProps {
+//     optional uint32 n_val = 1;
+//     optional bool allow_mult = 2;
+// }
 var decode_GetBucketResp = function(stream, res) {
     res = res || {props : {}};
     decodeLoop(stream, function(type, fieldnum) {
@@ -144,6 +183,11 @@ var decode_GetBucketResp = function(stream, res) {
     return res;
 }
 
+// message RpbMapRedResp {
+//     optional uint32 phase = 1;
+//     optional bytes response = 2;
+//     optional bool done = 3;
+// }
 var decode_MapReduceResp = function(stream, res) {
     res = res || {phase:0, response:[], done:false};
     decodeLoop(stream, function(type, fieldnum) {
@@ -158,6 +202,10 @@ var decode_MapReduceResp = function(stream, res) {
     return res;
 }
 
+// message RpbBucketProps {
+//     optional uint32 n_val = 1;
+//     optional bool allow_mult = 2;
+// }
 var decode_BucketProps = function(stream, res) {
     var len = readVarInt(stream);
     res = res || {n_val : 0, allow_mult: false};
@@ -171,6 +219,10 @@ var decode_BucketProps = function(stream, res) {
     return res;
 }
 
+// message RpbPair {
+//     required bytes key = 1;
+//     optional bytes value = 2;
+// }
 var decode_Pair = function(stream, res) {
     var len = readVarInt(stream);
     res = res || {key:"", value : "" };
@@ -185,6 +237,11 @@ var decode_Pair = function(stream, res) {
     return res;
 }
 
+// message RpbLink {
+//     optional bytes bucket = 1;
+//     optional bytes key = 2;
+//     optional bytes tag = 3;
+// }
 var decode_Link = function(stream, res) {
     var len = readVarInt(stream);
     res = res || {bucket: "", key:"", tag : "" };
@@ -201,6 +258,18 @@ var decode_Link = function(stream, res) {
     return res;
 }
 
+// message RpbContent {
+//     required bytes value = 1;
+//     optional bytes content_type = 2;     // the media type/format
+//     optional bytes charset = 3;
+//     optional bytes content_encoding = 4;
+//     optional bytes vtag = 5;
+//     repeated RpbLink links = 6;          // links to other resources
+//     optional uint32 last_mod = 7;
+//     optional uint32 last_mod_usecs = 8;
+//     repeated RpbPair usermeta = 9;       // user metadata stored with the object
+//     repeated RpbPair indexes = 10;
+// }
 var decode_Content = function(stream, res) {
     var len = readVarInt(stream);
     res = res || {value : "", content_type:"", charset:"", content_encoding:"", vtag:"", links:[], last_mod:0, last_mod_usecs:0, usermeta:[], indexes:[] };
@@ -232,6 +301,7 @@ var decode_Content = function(stream, res) {
 
 // ----------------------
 // Writing protocol buffers
+// ----------------------
 
 var addMessageHeader = function(buf, len, msg) {
     buf[0] = (len >> 24) & 0xFF;
@@ -289,6 +359,7 @@ var makeMessage = function(msg, params) {
 
 // ----------------------
 // Encoding Riak requests
+// ----------------------
 
 var encode_PingReq = function() {
     return new Buffer([0,0,0,1,1]);
@@ -298,6 +369,9 @@ var encode_GetClientIdReq = function() {
     return new Buffer([0,0,0,1,3]);
 }
 
+// message RpbSetClientIdReq {
+//     required bytes client_id = 1; // Client id to use for this connection
+// }
 var encode_SetClientIdReq = function(client_id) {
     return makeMessage(5, [client_id]);
 }
@@ -306,11 +380,35 @@ var encode_GetServerInfoReq = function() {
     return new Buffer([0,0,0,1,7]);
 }
 
+// message RpbGetReq {
+//     required bytes bucket = 1;
+//     required bytes key = 2;
+//     optional uint32 r = 3;
+//     optional uint32 pr = 4;
+//     optional bool basic_quorum = 5;
+//     optional bool notfound_ok = 6;
+//     optional bytes if_modified = 7;
+//     optional bool head = 8;
+//     optional bool deletedvclock = 9;
+// }
 var encode_GetReq = function(bucket, key, options) {
     options = options || {};
     return makeMessage(9, [bucket, key, options.r, options.pr, options.basic_quorum, options.notfound_ok, options.if_modified, options.head, options.deletedvclock]);
 }
 
+// message RpbPutReq {
+//     required bytes bucket = 1;
+//     optional bytes key = 2;
+//     optional bytes vclock = 3;
+//     required RpbContent content = 4;
+//     optional uint32 w = 5;
+//     optional uint32 dw = 6;
+//     optional bool return_body = 7;
+//     optional uint32 pw = 8;
+//     optional bool if_not_modified = 9;
+//     optional bool if_none_match = 10;
+//     optional bool return_head = 11;%
+// }
 var encode_PutReq = function(bucket, key, data, options) {
     options = options || {};
     options.content = options.content || {};
@@ -324,6 +422,17 @@ var encode_PutNokeyReq = function(bucket, data, options) {
     return encode_PutReq(bucket, undefined, data, options);
 }
 
+// message RpbDelReq {
+//     required bytes bucket = 1;
+//     required bytes key = 2;
+//     optional uint32 rw = 3;
+//     optional bytes vclock = 4;
+//     optional uint32 r = 5;
+//     optional uint32 w = 6;
+//     optional uint32 pr = 7;
+//     optional uint32 pw = 8;
+//     optional uint32 dw = 9;
+// }
 var encode_DeleteReq = function(bucket, key, options) {
     options = options || {};
     return makeMessage(11, [bucket, key, options.rw, options.vclock, options.r, options.w, options.pr, options.pw, options.dw ]);
@@ -333,22 +442,43 @@ var encode_ListBucketsReq = function() {
     return new Buffer([0,0,0,1,15]);
 }
 
+// message RpbListKeysReq {
+//     required bytes bucket = 1;
+// }
 var encode_ListKeysReq = function(bucket) {
     return makeMessage(17, [bucket]);
 }
 
+// message RpbGetBucketReq {
+//     required bytes bucket = 1;
+// }
 var encode_GetBucketReq = function(bucket) {
     return makeMessage(19, [bucket]);
 }
 
+// message RpbSetBucketReq {
+//     required bytes bucket = 1;
+//     required RpbBucketProps props = 2;
+// }
+// message RpbBucketProps {
+//     optional uint32 n_val = 1;
+//     optional bool allow_mult = 2;
+// }
 var encode_SetBucketReq = function(bucket, props) {
     return makeMessage(21, [bucket, [props.n_val, props.allow_mult]]);
 }
 
+// message RpbMapRedReq {
+//     required bytes request = 1;
+//     required bytes content_type = 2;
+// }
 var encode_MapReduceReq = function(request, content_type) {
     return makeMessage(23, [request, content_type]);
 }
 
+// ----------------------
+// The actual client object.
+// ----------------------
 
 var Client = function(host, port, cb) {
     this.state = 'disconnected';
@@ -361,6 +491,11 @@ Client.prototype.connect = function(host, port, cb) {
     this.queue = [];
     this.state = 'connecting';
     this.res = undefined;
+    this.pendingBuffers = [];
+    this.receivedTotalData = 0;
+    this.needTotalData = 0;
+
+
     var self = this;
     this.client = net.connect(this.port, this.host, function() {
         console.log('client connected at host', self.host, 'port', self.port);
@@ -402,53 +537,102 @@ Client.prototype.onEnd = function(cb) {
 
 Client.prototype.onData = function(data) {
     //console.log(data.toString('hex'));
+
+    // Accumulate into our buffer array of received packets
+    this.pendingBuffers.push(data);
+    this.receivedTotalData += data.length;
+
+    // If we have enough data (4 bytes needed) to read the length of next packet, do it.
+    if (this.needTotalData == 0 && this.receivedTotalData >= 4) {
+        // Consolidate all received data 
+        if (this.pendingBuffers.length > 1) {
+            this.pendingBuffers = [consolidateBuffers(this.pendingBuffers)];
+        }
+        this.needTotalData = this.pendingBuffers[0].readUInt32BE(0)+4;
+    }
+
+    // If we have at least a full packet, process it and keep the remainder (part of the next packet).
+    if (this.needTotalData > 0 && this.needTotalData <= this.receivedTotalData) {
+        if (this.pendingBuffers.length > 1) {
+            this.pendingBuffers = [consolidateBuffers(this.pendingBuffers)];
+        }
+        var remainder = this.processPackets(this.pendingBuffers[0]);
+        if (remainder) {
+            this.pendingBuffers = remainder;
+            this.receivedTotalData = remainder.length;
+        } else {
+            this.pendingBuffers = [];
+            this.receivedTotalData = 0;
+        }
+        this.needTotalData = 0;
+    }
+
+    //console.log(this.pendingBuffers, this.receivedTotalData, this.needTotalData);
+}
+
+// Process all the packets available in the buffer
+// Return the remainder of the buffer
+Client.prototype.processPackets = function(data) {
     var stream = {buf:data, idx:0};
-    while (stream.idx < data.length) {
+    while (stream.idx+4 < data.length) {
         var len = data.readUInt32BE(stream.idx);
+        if (stream.idx + 4 + len > data.length) {
+            return data.slice(stream.idx);
+        }
         var msg = data.readUInt8(stream.idx+4);
         stream.idx += 5;
-        stream.len = stream.idx + len-1;
-        //console.log("RECEIVED: message " + msg + ", length " + len);
-        var err = null;
-        if (msg == 0) {
-            this.res = decode_ErrorResp(stream, this.res);
-            err = this.res;
-            this.res = {};
-        } else if (msg == 2) {
-            this.res = {}; // nothing to decode for RpbPingResp 
-        } else if (msg == 4) {
-            this.res = decode_GetClientIdResp(stream, this.res);
-        } else if (msg == 6) {
-            this.res = {}; // nothing to decode for RpbSetClientIdResp 
-        } else if (msg == 8) {
-            this.res = decode_GetServerInfoResp(stream, this.res);
-        } else if (msg == 10) {
-            this.res = decode_GetResp(stream, this.res);
-        } else if (msg == 12) {
-            this.res = decode_PutResp(stream, this.res);
-        } else if (msg == 14) {
-            this.res = {}; // nothing to decode for RpbDelResp
-        } else if (msg == 16) {
-            this.res = decode_ListBucketsResp(stream, this.res);
-        } else if (msg == 18) {
-            this.res = decode_ListKeysResp(stream, this.res);
-        } else if (msg == 20) {
-            this.res = decode_GetBucketResp(stream, this.res);
-        } else if (msg == 22) {
-            this.res = {}; // nothing to decode for RpbSetBucketResp
-        } else if (msg == 24) {
-            this.res = decode_MapReduceResp(stream, this.res);
-        } else {
-            stream.idx += len;
-            this.res = {};
-        }
-        if (this.res && (this.res.done === undefined || this.res.done === true)) {
-            if (this.queue[0].cb) this.queue[0].cb(err, this.res);
-            this.queue.shift();
-            //console.log(err? err.errmsg : this.res);
-            this.res = undefined;
-            this.nextRequest();
-        }
+        this.processPacket(stream, msg, len);
+    }
+    if (stream.idx == data.length)
+        return null;
+    return data.slice(stream.idx);
+}
+
+// Process one packet
+Client.prototype.processPacket = function(stream, msg, len) {
+    stream.len = stream.idx + len-1;
+    //console.log("RECEIVED: message " + msg + ", length " + len);
+    var err = null;
+    if (msg == 0) {
+        this.res = decode_ErrorResp(stream, this.res);
+        err = this.res;
+        this.res = {};
+    } else if (msg == 2) {
+        this.res = {}; // nothing to decode for RpbPingResp 
+    } else if (msg == 4) {
+        this.res = decode_GetClientIdResp(stream, this.res);
+    } else if (msg == 6) {
+        this.res = {}; // nothing to decode for RpbSetClientIdResp 
+    } else if (msg == 8) {
+        this.res = decode_GetServerInfoResp(stream, this.res);
+    } else if (msg == 10) {
+        this.res = decode_GetResp(stream, this.res);
+    } else if (msg == 12) {
+        this.res = decode_PutResp(stream, this.res);
+    } else if (msg == 14) {
+        this.res = {}; // nothing to decode for RpbDelResp
+    } else if (msg == 16) {
+        this.res = decode_ListBucketsResp(stream, this.res);
+    } else if (msg == 18) {
+        this.res = decode_ListKeysResp(stream, this.res);
+    } else if (msg == 20) {
+        this.res = decode_GetBucketResp(stream, this.res);
+    } else if (msg == 22) {
+        this.res = {}; // nothing to decode for RpbSetBucketResp
+    } else if (msg == 24) {
+        this.res = decode_MapReduceResp(stream, this.res);
+    } else {
+        stream.idx += len;
+        this.res = {};
+    }
+    // If response was only one packet, or we have received all the packets for
+    // a multi-packet response, deliver the callback and trigger next request
+    if (this.res.done === undefined || this.res.done === true) {
+        if (this.queue[0].cb) this.queue[0].cb(err, this.res);
+        this.queue.shift();
+        //console.log(err? err.errmsg : this.res);
+        this.res = undefined;
+        this.nextRequest();
     }
 }
 
